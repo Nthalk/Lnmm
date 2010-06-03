38f75661d22f967d7118cf864fb04f10f9d990d3
Nthalk <phocis@gmail.com>
2010-06-03 06:14:04 -0700

    Fixed bug in observable

--- test/test-observable.js
+++ test/test-observable.js
@@ -1,40 +1,56 @@
 var Observable = require("../lib/Observable").Observable;
 
+
 exports.Observable = function(test){
-  test.expect(7);
+  test.expect(11);
   var o = new Observable();
   
   // Simple events
-   o.on("ev1",function(data1,data2){
-     test.ok(data1===1);
-     test.ok(data2===2);
-   });
-   o.fire("ev1",[1,2]);
+  o.on("ev1",function(data1,data2){
+    test.ok(data1===1);
+    test.ok(data2===2);
+  });
+  o.on("ev1",function(data1,data2){
+    test.ok(data1===1);
+    test.ok(data2===2);
+  });
+  
+  o.fire("ev1",[1,2]);
+  // More complicated
+  o.once("ev2",function(data){
+    test.ok(data==="only once!");
+  });
+  o.fire("ev2",["only once!"]);
+  o.fire("ev2",["happend twice!"]);
    
-   // More complicated
-   o.once("ev2",function(data){
-     test.ok(data==="only once!");
-   });
-   o.fire("ev2",["only once!"]);
-   o.fire("ev2",["only once!"]);
+  // Function relaying
+  var i = 0;
+  var fn = function(){return "fn-" + (i++);};
+  fn = o.relay("fn-spy",fn);
+  o.on("fn-spy",function(data){
+    test.ok(data==="fn-0");
+  });
+  fn();
    
-   // Function relaying
-   var i = 0;
-   var fn = function(){return "fn-" + (i++);};
-   fn = o.relay("fn-spy",fn);
-   o.on("fn-spy",function(data){
-     test.ok(data==="fn-0");
-   });
-   fn();
+  // Group scheduling
+  o.when(["article","session"],function(article,session){
+    test.ok(article[0] === "article");
+    test.ok(session[0] === "session");
+  });
+  
+  o.when(["template","session","article"],function(template,session,article){
+    test.ok(template[0] === "template");
+    test.ok(session[0] === "session");
+    test.ok(article[0] === "article");
+    test.done();
+  });
+  
+  
    
-   // Group scheduling
-   o.when(["template","session","article"],function(template,session,article){
-     test.ok(template[0] === "template");
-     test.ok(session[0] === "session");
-     test.ok(article[0] === "article");
-     test.done();
-   });
-   setTimeout(o.relay("template",function(){return "template";}),20);
-   setTimeout(o.relay("session",function(){return "session";}),20);
-   setTimeout(o.relay("article",function(){return "article";}),20);
+  setTimeout(o.relay("template",function(){
+    return "template";}),25);
+  setTimeout(o.relay("session",function(){
+    return "session";}),10);
+  setTimeout(o.relay("article",function(){
+    return "article";}),11);
 };
