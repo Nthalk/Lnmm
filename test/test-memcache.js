c73422e383294dc530c238f3d066c46bc98a7fe6
Nthalk <phocis@gmail.com>
2010-06-03 05:12:45 -0700

    removed wrong memcache dependency, udpated some tests, added a conf dir

--- test/test-memcache.js
+++ test/test-memcache.js
@@ -6,20 +6,22 @@ exports.Memcache = function(test){
   test.expect(1);
   mem.set("connect-test",1,function(value){});
   mem.get("connect-test",function(value){
-    test.ok(value);
+    test.ok(value == 1);
     test.done();
   });
 };
-
+var sys = require("sys");
 exports.MemcacheStress = function(test){
   var i = 0;
-  for(i = 0; i < 5000; i++){
-    mem.set(i,i,function(value){});
-    mem.get(i,function(value){});
-  }
-  test.expect(1);
-  mem.get(400,function(value){
-    test.ok(400);
-    test.done();
-  });
+  for(i = 0; i < 100; i++){(function(i){
+    var key = "memcache-test-"+i;
+    mem.set(key,i,function(){
+      mem.get(key,function(v){
+        mem.set(key,v^2,function(q){
+          sys.log(sys.inspect(arguments));
+        });
+      });
+    });
+  })(i);}
+  test.expect(101);
 };
--- test/test-mysql.js
+++ test/test-mysql.js
@@ -3,27 +3,27 @@ var mysql1 = new MySQL({pass:"m8db"});
 var mysql2 = new MySQL({pass:"m8db"});
 
 exports.MySQLStress = function(test){
+  var total = 200;
   var q;
-  for(i = 0; i < 10000; i++){
+  for(i = 0; i < total; i++){
     if(i%2===1){
       q = mysql1;
     }else{
       q = mysql2;
     }
-    var v = i%500;
-    test.expect(10000);
-    q.query("select ?;",[v],(function(t){return function(value){test.ok(t=value);};})(v));
+    test.expect(total);
+    q.query("select ?;",[i],(function(t){return function(value){test.ok(t=value);};})(i));
   }
-  var i = 0;
-  mysql1.query("select ?;",[v],function(){
-    i++;
-    if(i == 2){
+  var done = 0;
+  mysql1.query("select ?;",[i],function(){
+    done++;
+    if(done == 2){
       test.done();
     }
   });
-  mysql2.query("select ?;",[v],function(){
-    i++;
-    if(i == 2){
+  mysql2.query("select ?;",[i],function(){
+    done++;
+    if(done == 2){
       test.done();
     }
   });
